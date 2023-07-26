1721e8c8630dccd6c88b1c0d92ba95cbdc3ab7b0
Angelus-20 <edwinalexander20@gmail.com>
2023-07-25 21:48:08 -0400

    updated eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(array1, array2){
+  if (array1.length !== array2.length){
+      return false;
+  }
+  
+  for (let i = 0; i < eqArrays; i++){
+    if (array1[i] !== array2[i]){
+      return false;
+    }
+  }
+  return true;
+};
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
