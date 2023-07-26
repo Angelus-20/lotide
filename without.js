724ef4770a654ff6a66cb04491557dea827c1c2a
Angelus-20 <edwinalexander20@gmail.com>
2023-07-25 22:58:41 -0400

    added without

---
+++ without.js
@@ -0,0 +1,41 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+function without(array1, toExclude){
+  const newArray = [];
+
+  for( const element of array1){
+    if (!toExclude.includes(element)){
+      newArray.push(element);
+    }
+  }
+  return newArray;
+}
+
+// TEST CODE
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => should PASS
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+console.log(without([1, 16, 3, 45], [45])) // => [1, 16 ,3]
+console.log(without(["11", "2", "3"], [11, "2", "3"])) // => ["11"]
