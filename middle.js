11cd00a3e8176133d794e51457f591084d24be65
Angelus-20 <edwinalexander20@gmail.com>
2023-07-26 15:17:57 -0400

    looked for middle index info

---
+++ middle.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};const middle = (list) => {
+  if (list.length < 3) {
+      return []
+  }
+  
+  if (list.length % 2 === 0) {
+      const middleIndex1 = Math.floor(list.length / 2) - 1 // middleIndex in this case represents [i]
+      const middleIndex2 = Math.floor(list.length / 2)
+      const element1 = list[middleIndex1] // refers to that index on the list
+      const element2 = list[middleIndex2]
+      
+      return [element1, element2]
+  }
+  
+  if (list.length % 2 !== 0) {
+      const middleIndex = Math.floor(list.length / 2)
+      const element = list[middleIndex]
+      return [element]
+  }
+};
+
+console.log(middle([1, 2, 3, 4, 5])) //=> ['3'] middle position - 2
+console.log(middle(['a', 'b', 'c', 'd'])) //=> ['b', 'c'] middle position - 1, 2
+console.log(middle([1, 2, 3])) //=> [2]
+console.log(middle([1])) //=> []
+console.log(middle([])) //=> []
