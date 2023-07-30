22b67c82e497615d70970a5cc3210888c83f67b1
Angelus-20 <edwinalexander20@gmail.com>
2023-07-30 13:13:40 -0400

    implemented findKey and asserting with assertEqual

--- findKey.js
+++ findKey.js
@@ -6,9 +6,28 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// const findKey = function(obj, callBack) {
-//   for (const key in object) {
-//     const value = 
+findKey = {
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}
 
-//   }
-// }
+function stars(obj, stars) {
+  for (const key in obj) {
+    if (obj[key].stars === stars) {
+      return key;
+    }
+  }
+};
+
+const result1 = stars(findKey, 2);
+assertEqual(result1, "noma");
+const result4 = stars(findKey, 2);
+assertEqual(result4, "Ora");
+const result2 = stars(findKey, 3);
+assertEqual(result2, "Akaleri");
+const result3 = stars(findKey, 3);
+assertEqual(result3, "elBulli");
