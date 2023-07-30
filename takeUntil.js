83052d71f779a9146a8e0c4b042e9e8d33f358fe
Angelus-20 <edwinalexander20@gmail.com>
2023-07-30 11:49:44 -0400

    modified code

--- takeUntil.js
+++ takeUntil.js
@@ -21,20 +21,27 @@ const eqArrays = function(array1, array2) {
 
 
 const takeUntil = function(array, callback) {
-  // ...
+  const results = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    }
+    results.push(item);
+  }
+  return results;
+};
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const takeUntil = callback(array);
-const results1 = takeUntil(data1, x => x < 0);{
-};
-console.log(results1);
-}
-console.log('---');
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1); // Output: [ 1, 2, 5, 7, 2 ]
+
+console.log('--------');
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
 // [ 1, 2, 5, 7, 2 ]
 // --
-// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+// [ 'I've', 'been', 'to', 'Hollywood' ]
