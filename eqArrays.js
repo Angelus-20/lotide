622ccab10eba7ea00b659a9801da10ed51f7d721
Angelus-20 <edwinalexander20@gmail.com>
2023-07-31 14:56:39 -0400

    updated ver - code refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
@@ -19,8 +12,5 @@ const eqArrays = function(array1, array2) {
   return true;
 };
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+module.exports = eqArrays;
+
--- tail.js
+++ tail.js
@@ -1,9 +1,3 @@
-// const head = function(arr) {
-//   if (arr.length === 0) {
-//     return undefined;
-//   }
-//   return arr[0];
-// };
 
 const tail = function(arr) {
   if (arr.length === 0) {
@@ -11,15 +5,6 @@ const tail = function(arr) {
   }
   return arr.slice(1);
 };
-
-// // TEST CODE
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-//   } else {
-//     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-//   }
-// };
 
 
 module.exports = tail;
