a0d106c77a38e30c95a9f8acd98eeab9505a8dd4
Angelus-20 <edwinalexander20@gmail.com>
2023-07-31 14:48:47 -0400

    updated ver - code refactored

--- tail.js
+++ tail.js
@@ -1,9 +1,9 @@
-const head = function(arr) {
-  if (arr.length === 0) {
-    return undefined;
-  }
-  return arr[0];
-};
+// const head = function(arr) {
+//   if (arr.length === 0) {
+//     return undefined;
+//   }
+//   return arr[0];
+// };
 
 const tail = function(arr) {
   if (arr.length === 0) {
@@ -12,21 +12,14 @@ const tail = function(arr) {
   return arr.slice(1);
 };
 
-// TEST CODE
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+// // TEST CODE
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
-// Test cases for head
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), undefined);
 
-// Test cases for tail
-assertEqual(tail([5, 6, 7]), [6, 7]);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
-assertEqual(tail([]), []);
+module.exports = tail;
