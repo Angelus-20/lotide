8395404daba48678b57facd1a242f49d668298da
Angelus-20 <edwinalexander20@gmail.com>
2023-07-31 14:40:20 -0400

    updated ver

--- assertEqual.js
+++ assertEqual.js
@@ -7,9 +7,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("letters", "positions");
-assertEqual("boo", 3);
-assertEqual("scary", "scary");
+
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("letters", "positions");
+assertEqual("boo", 3);
+assertEqual("scary", "scary");
+
